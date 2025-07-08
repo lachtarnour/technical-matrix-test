4fb8147a62cfbc9f849aad8ee496b6db3049d148
Slany <webmaster@slany.work>
2025-07-08 16:41:13 +0200

    chore: init

---
+++ matrix.py
@@ -0,0 +1,28 @@
+class Matrix(object):
+    """
+    Classe pour représenter une matrice et réaliser les opérations de base :
+    addition, soustraction, multiplication et calcul du déterminant.
+    """
+    pass
+
+    # 1. initialize a matrix: ex.mat = Matrix([[1,2,3],[1,2,3]])
+
+    # 2. realize the basic operation add: ex. mat1 + mat2 = mat3
+
+    # 3. calculate the determinant of a matrix: ex. mat.det
+
+
+if __name__ == '__main__':
+    # Example
+
+    # Test for #1
+    # mat1 = Matrix([[1,2,3],[1,4,3],[1,4,4]])
+
+    # Test for #2
+    # mat2 = Matrix([[1,2,3],[1,4,3],[1,4,4]])
+    # mat3 = mat1 + mat2
+
+    # Test for #3
+    # print(mat1.det())
+    # print(mat3)
+    pass
